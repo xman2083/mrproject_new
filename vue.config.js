66da978541c0f5ef23fff9bf972cc12c4422e6c9
강단 <kkangdanni@sk.com>
2019-07-22 15:28:47 +0900

    config.js/ docker file add

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,22 @@
 module.exports = {
     lintOnSave: false,
-};
+    publicPath: '/sktl-inner-dev/meetingroom/kkangdanni',
+  
+    pwa: {
+      workboxPluginMode: "GenerateSW",
+      workboxOptions: {
+        navigateFallback: "/index.html",
+        runtimeCaching: [{
+          urlPattern: new RegExp('APU_URL'),
+          handler: 'networkFirst',
+          options: {
+            networkTimeoutSeconds: 20,
+            cacheName: 'sktl-meeting-room',
+            cacheableResponse: {
+              statuses: [0, 200],
+            },
+          },
+        }]
+      }
+    }
+  };
