9632839a2306033f48e9511e4ea479732ec7cf87
xman2083 <tabasco@tabascoui-MacBookPro.local>
2019-08-01 16:28:45 +0900

    merge

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,64 +1,62 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import store from '../store';
-import MeetingRoomBtn from '../views/MeetingRoomBtn';
-import Login from '../views/Login.vue';
+import Vue from "vue";
+import Router from "vue-router";
+import store from "../store";
+import MeetingRoomBtn from "../views/MeetingRoomBtn";
+import Login from "../views/Login.vue";
 
 // import { getUserFromCookie } from '../utils/cookies.js';
-import noticeView from '../views/noticeView.vue';
-import mainView from '../views/mainView.vue';
-import RstrMenuView from '../views/RstrMenuView.vue';
+import noticeView from "../views/noticeView.vue";
+import mainView from "../views/mainView.vue";
+import RstrMenuView from "../views/RstrMenuView.vue";
 
-
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
-    
     {
-      path: '/',
-      name: 'main',
+      path: "/",
+      name: "main",
       component: mainView,
       beforeEnter(to, from, next) {
-        store.getters['isLoggedIn'] ? next() : next('/login');
+        store.getters["isLoggedIn"] ? next() : next("/login");
       },
     },
 
-      {
-        path: '/meetingroombtn',
-        name: 'meetingroombtn',
-        component: MeetingRoomBtn,
-        beforeEnter(to, from, next) {
-          store.getters['isLoggedIn'] ? next() : next('/login');
-        },
+    {
+      path: "/meetingroombtn",
+      name: "meetingroombtn",
+      component: MeetingRoomBtn,
+      beforeEnter(to, from, next) {
+        store.getters["isLoggedIn"] ? next() : next("/login");
       },
+    },
 
     {
-      path: '/login',
-      name: 'login',
+      path: "/login",
+      name: "login",
       component: Login,
       beforeEnter(to, from, next) {
-        store.getters['isLoggedIn'] ? next('/') : next();
+        store.getters["isLoggedIn"] ? next("/") : next();
       },
     },
     {
-      path: '/notice',
-      name: 'notice',
+      path: "/notice",
+      name: "notice",
       component: noticeView,
       beforeEnter(to, from, next) {
-        store.getters['isLoggedIn'] ? next() : next('/login');
+        store.getters["isLoggedIn"] ? next() : next("/login");
       },
     },
     {
-      path: '/rstrmenu',
-      name: 'rstrmenu',
+      path: "/rstrmenu",
+      name: "rstrmenu",
       component: RstrMenuView,
       beforeEnter(to, from, next) {
-        store.getters['isLoggedIn'] ? next() : next('/login');
+        store.getters["isLoggedIn"] ? next() : next("/login");
       },
     },
-  ]
-})
+  ],
+});
 /*
 function beforeEnter(to, from, next) {
   if (store.getters['isLoggedIn'] || getUserFromCookie()) {
