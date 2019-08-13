5233055cc94470f7cfd0eefec846a0ad90143d3a
xman2083 <tabasco@tabascoui-MacBookPro.local>
2019-08-13 11:31:17 +0900

    polyfill 적용

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   presets: [
-    '@vue/app'
-  ]
-}
+    [
+      "@vue/app",
+      {
+        // targets: { ie: 8 },
+        // debug: true,
+        useBuiltIns: "entry",
+      },
+    ],
+  ],
+};
