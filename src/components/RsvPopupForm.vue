6d8f7efaf0c01d02cdd55b544fd55553dbff18ee
xman2083 <tabasco@tabascoui-MacBookPro.local>
2019-08-19 09:38:41 +0900

    예약 팝업 오류 수정

--- src/components/RsvPopupForm.vue
+++ src/components/RsvPopupForm.vue
@@ -12,6 +12,7 @@
         style="color:grey !important;"
       >&nbsp;&nbsp;{{this.currCell[0].name}}&nbsp;&nbsp;</span>
       <span class="grey--text subtitle-1">{{this.date}}</span>
+      {{cell_time}}
     </v-card-title>
 
     <v-divider style="margin:0px;"></v-divider>
@@ -352,11 +353,7 @@ export default {
         this.$emit("clearRsv");
       }
     });
-  },
-  computed: {
-    ...mapGetters(["getRsvDataStore"])
-  },
-  created() {}
+  }
 };
 </script>
 
